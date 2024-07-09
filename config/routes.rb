e72a60ac2dca6bbcbf5ccef695ce066a045c3638
yuta369 <y.m.36912@gmail.com>
2024-07-09 11:36:08 +0000

    [Update] Webpacker の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,67 @@
-Rails.application.routes.draw do
-  # Deviseルーティング（Adminユーザー）
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+# Rails.application.routes.draw do
+#   # Deviseルーティング（Adminユーザー）
+#   devise_for :admin_users, ActiveAdmin::Devise.config
+#   ActiveAdmin.routes(self)
   
-  # Deviseルーティング（一般ユーザー）
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords',
-    confirmations: 'users/confirmations',
-    unlocks: 'users/unlocks',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+#   # Deviseルーティング（一般ユーザー）
+#   devise_for :users, controllers: {
+#     sessions: 'users/sessions',
+#     registrations: 'users/registrations',
+#     passwords: 'users/passwords',
+#     confirmations: 'users/confirmations',
+#     unlocks: 'users/unlocks',
+#     omniauth_callbacks: 'users/omniauth_callbacks'
+#   }
 
-  # 静的ページ
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
-  get 'terms_of_service', to: 'static_pages#terms_of_service'
-  get 'help', to: 'static_pages#help'
-  get 'contact', to: 'static_pages#contact'
-  post 'contact/confirm', to: 'static_pages#confirm'
-  get 'contact/complete', to: 'static_pages#complete'
+#   # 静的ページ
+#   get 'privacy_policy', to: 'static_pages#privacy_policy'
+#   get 'terms_of_service', to: 'static_pages#terms_of_service'
+#   get 'help', to: 'static_pages#help'
+#   get 'contact', to: 'static_pages#contact'
+#   post 'contact/confirm', to: 'static_pages#confirm'
+#   get 'contact/complete', to: 'static_pages#complete'
 
-  # ホームページ
-  root to: 'home#index'
-  get 'about', to: 'home#about'
+#   # ホームページ
+#   root to: 'home#index'
+#   get 'about', to: 'home#about'
 
-  # ユーザー関連
-  resources :users, only: [:index, :show, :edit, :update, :destroy] do
-    member do
-      post 'follow', to: 'follows#create'
-      delete 'unfollow', to: 'follows#destroy'
-    end
-  end
+#   # ユーザー関連
+#   resources :users, only: [:index, :show, :edit, :update, :destroy] do
+#     member do
+#       post 'follow', to: 'follows#create'
+#       delete 'unfollow', to: 'follows#destroy'
+#     end
+#   end
 
-  # カフェ関連
-  resources :cafes do
-    resources :reviews, only: [:create, :edit, :update, :destroy]
-    resources :reservations, only: [:create, :show, :edit, :update, :destroy]
-    member do
-      post 'favorite', to: 'favorites#create'
-      delete 'unfavorite', to: 'favorites#destroy'
-    end
-  end
+#   # カフェ関連
+#   resources :cafes do
+#     resources :reviews, only: [:create, :edit, :update, :destroy]
+#     resources :reservations, only: [:create, :show, :edit, :update, :destroy]
+#     member do
+#       post 'favorite', to: 'favorites#create'
+#       delete 'unfavorite', to: 'favorites#destroy'
+#     end
+#   end
 
-  # レビュー関連
-  resources :reviews, only: [] do
-    resources :comments, only: [:create, :edit, :update, :destroy]
-  end
+#   # レビュー関連
+#   resources :reviews, only: [] do
+#     resources :comments, only: [:create, :edit, :update, :destroy]
+#   end
 
-  # 検索関連
-  get 'search', to: 'searches#index'
-  post 'search', to: 'searches#create'
+#   # 検索関連
+#   get 'search', to: 'searches#index'
+#   post 'search', to: 'searches#create'
 
-  # 管理者関連
-  namespace :admin do
-    get 'dashboard', to: 'admin#dashboard'
-    resources :users, only: [:index, :destroy]
-    resources :cafes, only: [:index, :destroy]
-    resources :reviews, only: [:index, :destroy]
-    resources :comments, only: [:index, :destroy]
-  end
+#   # 管理者関連
+#   namespace :admin do
+#     get 'dashboard', to: 'admin#dashboard'
+#     resources :users, only: [:index, :destroy]
+#     resources :cafes, only: [:index, :destroy]
+#     resources :reviews, only: [:index, :destroy]
+#     resources :comments, only: [:index, :destroy]
+#   end
 
-  # 削除確認ページ
-  get 'confirmations/:resource/:id', to: 'confirmations#show', as: 'confirm_delete'
-  delete 'confirmations/:resource/:id', to: 'confirmations#destroy'
-end
+#   # 削除確認ページ
+#   get 'confirmations/:resource/:id', to: 'confirmations#show', as: 'confirm_delete'
+#   delete 'confirmations/:resource/:id', to: 'confirmations#destroy'
+# end
