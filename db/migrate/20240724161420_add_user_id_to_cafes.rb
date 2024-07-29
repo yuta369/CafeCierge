d13265c50ae8b80d09c09e0b38631ea9c2673ed9
EC2 Default User <ec2-user@ip-172-31-15-242.ap-northeast-1.compute.internal>
2024-07-29 10:35:20 +0000

    [fix] remove migration_files

--- db/migrate/20240724161420_add_user_id_to_cafes.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToCafes < ActiveRecord::Migration[6.1]
-  def change
-    add_column :cafes, :user_id, :integer
-    add_index :cafes, :user_id
-    add_foreign_key :cafes, :users
-  end
-end
