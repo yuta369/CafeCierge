97f3ebfb58e741ad79b48c8d1e9e4d01d7e4bdd0
yuta369 <y.m.36912@gmail.com>
2024-09-19 15:15:28 +0000

    [Remove] Unnecessary line

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,7 +2,7 @@ class Tagging < ActsAsTaggableOn::Tagging
   belongs_to :tag
   belongs_to :taggable, polymorphic: true
   belongs_to :tagger, polymorphic: true, optional: true
-  
+
   validates :tag_id, presence: true
   validates :taggable_id, presence: true
   validates :taggable_type, presence: true
